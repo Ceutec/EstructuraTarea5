78723b51b17a7c2265ffc35cfc3128d82a5b0cd8
Turupawn <ahmed.hn.43@gmail.com>
2015-01-28 18:01:24 -0600

    2015

--- Evaluador.cpp
+++ Evaluador.cpp
@@ -34,26 +34,42 @@ void evaluar()
         && obtenerDistanciaAristaDirecta(grafo,2,1)==infinito
         )
     {
-        nota+=3;
+        nota+=2;
         cout<<"Correcto"<<endl;
     }else
     {
         cout<<"Incorrecto"<<endl;
     }
 
+    cout<<"Ejercicio obtenerListaDeAristas():\t";
+
+    if(obtenerListaDeAristas(grafo,3).size()==1 && obtenerListaDeAristas(grafo,3)[0]==4
+    && obtenerListaDeAristas(grafo,2).size()==1 && obtenerListaDeAristas(grafo,2)[0]==4
+    && obtenerListaDeAristas(grafo,1).size()==2 && (obtenerListaDeAristas(grafo,1)[0]==3 || obtenerListaDeAristas(grafo,1)[0]==4)
+    && obtenerListaDeAristas(grafo,1).size()==2 && (obtenerListaDeAristas(grafo,1)[1]==3 || obtenerListaDeAristas(grafo,1)[1]==4)
+    && obtenerListaDeAristas(grafo,0).size()==3 && (obtenerListaDeAristas(grafo,0)[0]==1 || obtenerListaDeAristas(grafo,0)[0]==2 || obtenerListaDeAristas(grafo,0)[0]==3)
+    && obtenerListaDeAristas(grafo,0).size()==3 && (obtenerListaDeAristas(grafo,0)[1]==1 || obtenerListaDeAristas(grafo,0)[1]==2 || obtenerListaDeAristas(grafo,0)[1]==3)
+    && obtenerListaDeAristas(grafo,0).size()==3 && (obtenerListaDeAristas(grafo,0)[2]==1 || obtenerListaDeAristas(grafo,0)[2]==2 || obtenerListaDeAristas(grafo,0)[2]==3))
+    {
+        nota+=2;
+        cout<<"Correcto"<<endl;
+    }else
+    {
+        cout<<"Incorrecto"<<endl;
+    }
 
     cout<<"Ejercicio existeCamino():\t\t";
     double nota_existe = 0;
     if(!existeCamino(grafo,4,1,5) && existeCamino(grafo,1,4,5) && !existeCamino(grafo,3,1,5) && existeCamino(grafo,2,4,5))
     {
-        nota_existe+=2;
+        nota_existe+=1.5;
     }
     if(existeCamino(grafo,0,4,5) && !existeCamino(grafo,4,1,5) && existeCamino(grafo,1,4,5) && !existeCamino(grafo,3,1,5) && existeCamino(grafo,2,4,5))
     {
-        nota_existe+=2;
+        nota_existe+=1.5;
 
     nota+=nota_existe;
-    if(nota==3,5)
+    if(nota_existe==1.5)
     {
         cout<<"Correcto"<<endl;
     }
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int infinito = 9999;
 
 //Dado: un grafo con 5 nodos implementado en una tabla (arreglo bidimensional)
 //Dado: las columnas de la tabla representan el inicio y las filas el destino
+//Dado: las columnas y las filas se representan en el arreglo de la siguiente manera: [columnas][filas]
 //Dado: las aristas tienen un valor de 0 si apuntan a ellas, infinito si no esta conectada y de lo contrario significa que sí hay una conexion entre los nodos
 
 //Devuelve la distancia (o peso) de una arista dado sus dos nodos (inicio y destino)
@@ -15,6 +16,12 @@ int infinito = 9999;
 int obtenerDistanciaAristaDirecta(int grafo[5][5], int inicio, int destino)
 {
     return -1;
+}
+
+vector<int> obtenerListaDeAristas(int grafo[5][5], int inicio)
+{
+    vector<int> respuesta;
+    return respuesta;
 }
 
 //Devuelve verdadero si existe al menos un camino para llegar desde el nodo inicio hasta el nodo destino dada una profundidad maxima de recorrido
